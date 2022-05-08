adccf4747048b414a03c560d6a15f4361a2bf5e5
Samantha Neely <samneely.dev@gmail.com>
2022-05-08 13:04:02 -0500

    set up site pages

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+  return (
+      <Layout>
+        <section>
+            <h1>About Me</h1>
+            <p>Laboris elit labore culpa esse in elit voluptate cupidatat. Deserunt non voluptate aute commodo do adipisicing in dolor pariatur. Ad pariatur ullamco mollit exercitation laboris pariatur ullamco veniam irure culpa est. Ad culpa consequat non laborum aliquip non deserunt mollit et commodo ullamco nulla. Pariatur occaecat nostrud elit elit adipisicing quis veniam anim labore ad. Exercitation tempor excepteur ex quis pariatur consectetur Lorem aute.</p>
+        </section>
+      </Layout>
+  )
+}
