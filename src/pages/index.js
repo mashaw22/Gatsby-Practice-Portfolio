30d783678d2560a97180e230cee7de84da786cbd
Samantha Neely <samneely.dev@gmail.com>
2022-05-09 07:25:57 -0500

    added template pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
 import {header, btn} from "../styles/home.module.css"
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-export default function Home() {
+export default function Home({data}) {
+  console.log(data)
   return (
     <Layout>
       <section className={header}>
@@ -13,7 +15,18 @@ export default function Home() {
           <p>UX designer based in Texas.</p>
           <Link to="/projects" className={btn}>My Portfolio</Link>
         </div>
+        <GatsbyImage image={getImage(data.file.childImageSharp.gatsbyImageData)}/>
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: {eq: "banner.png"}) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: AUTO)
+      }
+    }
+  }
+`
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,49 @@
+import { graphql, Link } from 'gatsby'
 import React from 'react'
 import Layout from '../../components/Layout'
-import {portfolio} from "../../styles/projects.module.css"
+import * as styles from "../../styles/projects.module.css"
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-export default function Projects() {
+export default function Projects({ data }) {
+    const projects = data.projects.nodes
+
   return (
       <Layout>
-        <div className={portfolio}>
+        <div className={styles.portfolio}>
             <h2>Portfolio Projects</h2>
             <h3>Below is a list of my portfolio projects.</h3>
+            <div className={styles.projects}>
+                {projects.map(project => (
+                    <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+                        <div>
+                            <GatsbyImage image={getImage(project.frontmatter.thumb.childImageSharp.gatsbyImageData)} alt="banner"/>
+                            <h3>{project.frontmatter.title}</h3>
+                            <p>{project.frontmatter.stack}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
       </Layout>
   )
 }
+
+export const query = graphql`
+query ProjectsPage {
+    projects: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+          thumb {
+            childImageSharp {
+              gatsbyImageData(layout: FULL_WIDTH, formats: AUTO, placeholder: BLURRED)
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
